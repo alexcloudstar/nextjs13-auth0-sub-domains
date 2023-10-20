03203b7949cb9456a28b0b1bcdc1a7e50837b911
alexcloudstar <alexcloudstar@gmail.com>
2023-10-20 19:18:03 +0300

    add: Auth0 package, configure & add login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Homepage
+    <main className='flex h-full flex-col items-center justify-between p-24'>
+      <div className='flex flex-col justify-center items-center h-full'>
+        <h1 className='text-3xl mb-2'>Hi there! 👋🏻</h1>
+        <a
+          href='/api/auth/login'
+          className='bg-[#00b2ff] text-white py-1.5 px-4 rounded-md text-xl'
+        >
+          Login
+        </a>
+      </div>
     </main>
   );
 }
