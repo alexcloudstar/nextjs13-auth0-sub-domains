a191ad58000b290cf1220ae518ec841767d83fdd
alexcloudstar <alexcloudstar@gmail.com>
2025-01-20 22:04:51 +0200

    fix: dynamic http protocol based on NODE_ENV

--- app/lib/auth0.ts
+++ app/lib/auth0.ts
@@ -2,10 +2,9 @@ import { initAuth0 } from '@auth0/nextjs-auth0';
 
 export const initializeAuth0 = (req: any): ReturnType<typeof initAuth0> => {
   return initAuth0({
-    // baseURL: `${
-    //   process.env.NODE_ENV === 'development' ? 'http' : 'https'
-    // }://${req.headers.get('host')}`,
-    baseURL: `http://${req.headers.get('host')}`,
+    baseURL: `${
+       process.env.NODE_ENV === 'development' ? 'http' : 'https'
+     }://${req.headers.get('host')}`,
     secret: process.env.AUTH0_SECRET,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
     clientID: process.env.AUTH0_CLIENT_ID,
